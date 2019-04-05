70f7d5390026eae5a988db8e1234d8524235aaed
Jayson Vibandor <jayson.vibandor@gmail.com>
2019-04-05 12:10:06 +0800

    Review Example
    added review example

--- algorithm/search/problems/find-max-appearing-element-in-a-list/main.go
+++ algorithm/search/problems/find-max-appearing-element-in-a-list/main.go
@@ -26,6 +26,34 @@ func getMax(data []int) int {
 	return max
 }
 
+// Brute Force(Iteration)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+func getMax1Review(data []int) int {
+	size := len(data)
+	max := data[0]
+	count := 1
+	maxCount := 1
+
+	// Find the items that has highest
+	// frequency count.
+	for i := 0; i < size; i++ {
+		count = 1
+		for j := i+1; j < size; j++ {
+			if data[i] == data[j] {
+				count++
+			}
+		}
+
+		if count > maxCount {
+			max = data[i]
+			maxCount = count
+		}
+	}
+
+	return max
+}
+
 func getMax2(data []int) int {
 	sort.Ints(data)
 	size := len(data)
@@ -35,7 +63,7 @@ func getMax2(data []int) int {
 	currCount := 1
 	//fmt.Println(curr, max)
 	for i := 1; i < size; i++ {
-		if data[i] == data[i-1] {
+		if data[i] == data[i-1] { //
 			currCount++
 		} else {
 			currCount = 1
@@ -44,6 +72,31 @@ func getMax2(data []int) int {
 		if currCount > maxCount {
 			maxCount = currCount
 			max = curr
+		}
+	}
+	return max
+}
+
+func getMax2Review(data []int) int {
+	sort.Ints(data)
+	size := len(data)
+	max := data[0] // initial max
+	maxCount := 1
+	currentSelected := data[0]
+	currentCount := 1
+
+	for i := 1; i < size; i++ {
+		if data[i] == data[i-1] {
+			currentCount++
+		} else {
+			// New item to be compared.
+			currentCount = 1
+			currentSelected = data[i]
+		}
+
+		if currentCount > maxCount {
+			maxCount = currentCount
+			max = currentSelected
 		}
 	}
 	return max
@@ -80,7 +133,7 @@ func getMax3V2(data []int, dataRange int) int {
 }
 
 func main() {
-	arr := []int{7, 2, 1, 29, 29,29,3, 2,5,2,3,3,3,7,9,29,19, 29, 29, 3, 3, 3, 1,1,1,1,1,1}
-	max := getMax3(arr, 30)
+	arr := []int{7, 2, 1, 29, 29,29,3, 2,5,2,3,3,3, 3, 3, 3, 3,7,9,29,19, 29, 29, 3, 3, 3, 1,1,1,1,1,1}
+	max := getMax1Review(arr)
 	fmt.Println(max)
 }
